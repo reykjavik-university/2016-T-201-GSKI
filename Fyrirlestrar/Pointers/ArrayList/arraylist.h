d7c4f22a60eb6942ec0b6360f7d9593fd8484dd2
Hrafn Loftsson <hrafn@ru.is>
2016-01-11 18:12:24 +0000

    Removes code from arraylist.h and arraylist.cpp

--- Fyrirlestrar/Pointers/ArrayList/arraylist.h
+++ Fyrirlestrar/Pointers/ArrayList/arraylist.h
@@ -4,19 +4,8 @@
 class ArrayList
 {
     public:
-        ArrayList(int size);        // Constructor
-        ~ArrayList();               // Destructor
-        void append (int elem);     // Appends an element at the end of the list
-        void next();                // Moves the current position one step right
-        void moveToStart();         // Set the current position to the start of the list
-        int currPos() const;        // Returns the position of the current element
-        int length() const;         // Returns the current length of the list
-        int value() const;          //Returns the current element
 
     private:
-        int maxSize;        // Maximum size of list
-        int currSize;       // Current number of list items
-        int currElemPos;    // Position of the current element of the list
         int* arr;           // A pointer to the array holding the list elements
 };
 
