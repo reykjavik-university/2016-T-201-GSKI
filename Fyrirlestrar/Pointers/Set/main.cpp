527655a5bcf9e3c48e17f86b5a2ce1240cb1acfa
Hrafn Loftsson <hrafn@ru.is>
2016-01-11 16:27:35 +0000

    Changes the main program for Set and ArrayOfSets

--- Fyrirlestrar/Pointers/Set/main.cpp
+++ Fyrirlestrar/Pointers/Set/main.cpp
@@ -2,23 +2,43 @@
 #include "set.h"
 using namespace std;
 
+typedef Set* SetPtr;
+
+void printSet(SetPtr set) {
+    cout << *set << endl;
+}
+
 int main()
 {
-    Set mySet;
+    SetPtr set1;
+    SetPtr set2;
 
+    set1 = new Set();
     for (int i = 0; i < 10; i++) {
-        mySet.insert(i);
-        mySet.insert(i);
+        set1->insert(i);
+        set1->insert(i);
     }
 
-    if (mySet.exist(4))
+    if (set1->exist(4))
         cout << "Element exists!" << endl;
 
-    mySet.remove(4);
-    mySet.remove(7);
-    mySet.remove(12);
+    set1->remove(4);
+    set1->remove(7);
+    set1->remove(12);
 
-    cout << mySet << endl;
+    printSet(set1);
+
+    set2 = set1;
+    printSet(set2);
+
+    set2 = new Set();
+    for (int i = 1; i < 5; i++) {
+        set2->insert(i*2);
+    }
+    printSet(set2);
+
+    delete set1;
+    delete set2;
 
     return 0;
 }
