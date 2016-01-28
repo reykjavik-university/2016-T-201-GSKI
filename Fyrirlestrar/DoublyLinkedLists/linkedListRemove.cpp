5f78a789bbac7d7802ad8942f5a00e2d14638109
Hrafn Loftsson <hrafn@ru.is>
2016-01-28 15:49:48 +0000

    Changes linked lists classes example, and doubly linked lists, in accordance to changes made in class

--- Fyrirlestrar/DoublyLinkedLists/linkedListRemove.cpp
+++ Fyrirlestrar/DoublyLinkedLists/linkedListRemove.cpp
@@ -13,6 +13,8 @@ struct Node
 typedef Node* NodePtr;
 
 void headInsert(NodePtr& head, string item, int count);
+void remove(NodePtr& head, NodePtr discard);
+NodePtr search(NodePtr head, string item);
 void printList(NodePtr head);
 void printListReverse(NodePtr head);
 void printNode(NodePtr node);
@@ -41,7 +43,24 @@ int main()
     cout << "Reverse order: " << endl;
     printListReverse(head);
 
+    NodePtr six = search(head, "6");
+    remove(head, six);
+    cout << "After removing six" << endl;
+    printList(head);
+
+    remove(head, head);
+    printList(head);
+
     deleteList(head);
+}
+
+NodePtr search(NodePtr head, string item)
+{
+    for(NodePtr iter = head; iter != NULL; iter = iter->forwardLink) {
+        if (iter->item == item)
+            return iter;
+    }
+    return NULL;
 }
 
 void headInsert(NodePtr& head, string item, int count)
@@ -57,6 +76,24 @@ void headInsert(NodePtr& head, string item, int count)
 
     tmpPtr->backLink = NULL;
     head = tmpPtr;
+}
+
+void remove(NodePtr& head, NodePtr discard) {
+
+    // If removing first node of list
+    if (discard->backLink == NULL)
+    {
+        head = discard->forwardLink;
+        if (discard->forwardLink != NULL)
+            (discard->forwardLink)->backLink = NULL;
+    }
+
+    else {
+        (discard->backLink)->forwardLink = discard->forwardLink;
+        if (discard->forwardLink != NULL)
+            (discard->forwardLink)->backLink = discard->backLink;
+    }
+    delete discard;
 }
 
 void printList(NodePtr head)
--- Fyrirlestrar/LinkedLists/LinkedListsClasses/List.cpp
+++ Fyrirlestrar/LinkedLists/LinkedListsClasses/List.cpp
@@ -8,43 +8,56 @@ List<T>::List() {
 template <class T>
 List<T>::~List() {
     cout << "Destructor running!\n";
-
+    removeAll();
 }
 
 template <class T>
 Node<T>* List<T>::getHead() const
 {
-
+    return head;
 }
 
 template <class T>
 Node<T>* List<T>::search(T item)
 {
-
+    for (Node<T>* iter = head; iter != NULL; iter = iter->getLink()) {
+        if (iter->getData() == item)
+            return iter;
+    }
     return NULL;
 }
 
 template <class T>
-void List<T>::headInsert(T value) {
-
-
+void List<T>::headInsert(T value)
+{
+    Node<T>* newNode = new Node<T>(value, head);
+    head = newNode;
 }
 
 template <class T>
-void List<T>::insert(Node<T>* afterMe, T value) {
-
-
+void List<T>::insert(Node<T>* afterMe, T value)
+{
+    Node<T>* newNode = new Node<T>(value, afterMe->getLink());
+    afterMe->setLink(newNode);
 }
 
 template <class T>
-void List<T>::remove(Node<T>* before, Node<T>* discard) {
-
-
+void List<T>::remove(Node<T>* before, Node<T>* discard)
+{
+    before->setLink(discard->getLink());
+    delete discard;
 }
 
 template <class T>
 void List<T>::removeAll()
 {
+    Node<T>* here = head;
+    while (here != NULL) {
+        Node<T>* tmpPtr = here->getLink();
+        delete here;
+        here = tmpPtr;
+    }
+    head = NULL;
 
 }
 
@@ -52,6 +65,11 @@ void List<T>::removeAll()
 template <class T>
 ostream& operator<< (ostream& out, List<T>* lis)
 {
+    for(Node<T>* iter = lis->head; iter != NULL; iter = iter->getLink())
+    {
+        out << iter << endl;
+    }
+    cout << endl;
 
     return out;
 }
