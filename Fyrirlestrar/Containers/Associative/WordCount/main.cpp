20e5899c341f4f17c38cea19c1106d44141bfc6a
hrafnl <hrafnl@gmail.com>
2016-03-13 12:22:23 +0000

    Updates the description for Vika10 and adds examples for associative containers

---
+++ Fyrirlestrar/Containers/Associative/WordCount/main.cpp
@@ -0,0 +1,5 @@
+// Word frequencies -- using map
+
+// Write a program that read words from standard input (until the word "QUIT")
+// and stores the word frequencies in a map
+// Finally use an iterator to display the reuslts
